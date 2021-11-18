d819eb0beea73c9bfe7891a61f68a6c0b2d7c4cb
Wittawas Phaisanwongdee <wittawas.phaisanwongdee@scb.co.th>
2021-11-18 20:41:21 +0700

    API Boy

--- src/main/java/th/co/scb/restapi/controller/ScbWeFixedAgentController.java
+++ src/main/java/th/co/scb/restapi/controller/ScbWeFixedAgentController.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import th.co.scb.restapi.model.ScbReportAgentSaveRequest;
-import th.co.scb.restapi.model.ScbReportSaveRequest;
 import th.co.scb.restapi.model.ScbReportSaveResponse;
 import th.co.scb.restapi.service.ScbWebFixedAgentService;
-import th.co.scb.restapi.service.ScbWebFixedService;
 
 @Slf4j
 @RestController
